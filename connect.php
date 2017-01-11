b7b1a45f02788a98159928d5fc28928fe7142555
charif98 <charifaffaoui@outlook.com>
2017-01-11 15:57:27 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php  
 try {
-$conn = new PDO('mysql:host=localhost; dbname=u519097_portfolio',"u519097_root" ,"Zgvo5l89");
+$conn = new PDO('mysql:host=localhost; dbname=xxxxx',"xxxxx" ,"xxxxx");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOExpeption $e){
 	echo "error: " .$e->getMessage();
