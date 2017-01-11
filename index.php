c1e638504789574ebd7b37f335753fd0e92c276b
charif98 <charifaffaoui@outlook.com>
2017-01-11 15:57:46 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,476 @@
+<?php  
+
+include ("connect.php");
+
+session_start();
+
+if (isset($_GET['action']) && $_GET['action'] == "logout"){
+  session_destroy();
+  header("location: portfolio1.php");
+}
+
+
+
+
+if(isset($_POST['loginform'])){ 
+
+  $gebruikersnaam = filter_var($_POST['gebruikersnaam'],FILTER_SANITIZE_STRING);
+  $wachtwoord = filter_var($_POST['wachtwoord'],FILTER_SANITIZE_STRING);
+
+  $query = $conn->prepare("
+    SELECT COUNT(`ID`) FROM `users`
+    WHERE `gebruikersnaam` = :gebruikersnaam
+    AND `wachtwoord` = :wachtwoord
+  "); 
+  $query->bindValue(':gebruikersnaam', $gebruikersnaam);
+  $query->bindValue(':wachtwoord', $wachtwoord);
+  $query->execute();
+  $count = $query->fetchColumn();
+
+  if($count == "1"){
+    $_SESSION['login_user'] = $gebruikersnaam;
+    header('Location: portfolio1.php');
+    exit;
+  } else{
+    echo "verkeerde gebruikersnaam";
+  }
+}
+
+// register
+if(isset($_POST['registerform'])){ 
+
+  $gebruikersnaam = filter_var($_POST['gebruikersnaam'],
+    FILTER_SANITIZE_STRING);
+  $wachtwoord = filter_var( $_POST['wachtwoord'],
+    FILTER_SANITIZE_STRING);
+  // anti hack filter
+
+  $query = $conn->prepare("
+    INSERT INTO `users` (gebruikersnaam,wachtwoord) 
+    VALUES (:gebruikersnaam, :wachtwoord)
+    ");
+
+  $query->execute(array('gebruikersnaam' => $gebruikersnaam,
+    'wachtwoord' => $wachtwoord));
+
+  if($query){
+    echo "gelukt";
+  } else {
+    echo "nope";
+  }
+}
+
+
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>C.C portfolio</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  body {
+      font: 400 15px/1.8 Lato, sans-serif;
+      color: #777;
+  }
+  h3, h4 {
+      margin: 10px 0 30px 0;
+      letter-spacing: 10px;
+      font-size: 20px;
+      color: #111;
+  }
+  .container {
+      padding: 80px 120px;
+  }
+  .person {
+      border: 10px solid transparent;
+      margin-bottom: 25px;
+      width: 80%;
+      height: 80%;
+      opacity: 0.7;
+  }
+  .person:hover {
+      border-color: #f1f1f1;
+  }
+  .carousel-inner img {
+      -webkit-filter: grayscale(90%);
+      filter: grayscale(90%); /* make all photos black and white */
+      width: 100%; /* Set width to 100% */
+      margin: auto;
+  }
+  .carousel-caption h3 {
+      color: #fff !important;
+  }
+  @media (max-width: 600px) {
+    .carousel-caption {
+      display: none; /* Hide the carousel text when the screen is less than 600 pixels wide */
+    }
+  }
+  .bg-1 {
+      background: #2d2d30;
+      color: #bdbdbd;
+  }
+  .bg-1 h3 {color: #fff;}
+  .bg-1 p {font-style: italic;}
+  .list-group-item:first-child {
+      border-top-right-radius: 0;
+      border-top-left-radius: 0;
+  }
+  .list-group-item:last-child {
+      border-bottom-right-radius: 0;
+      border-bottom-left-radius: 0;
+  }
+  .thumbnail {
+      padding: 0 0 15px 0;
+      border: none;
+      border-radius: 0;
+  }
+  .thumbnail p {
+      margin-top: 15px;
+      color: #555;
+  }
+  .btn {
+      padding: 10px 20px;
+      background-color: #333;
+      color: #f1f1f1;
+      border-radius: 0;
+      transition: .2s;
+  }
+  .btn:hover, .btn:focus {
+      border: 1px solid #333;
+      background-color: #fff;
+      color: #000;
+  }
+  .modal-header, h4, .close {
+      background-color: #333;
+      color: #fff !important;
+      text-align: center;
+      font-size: 30px;
+  }
+  .modal-header, .modal-body {
+      padding: 40px 50px;
+  }
+  .nav-tabs li a {
+      color: #777;
+  }
+  #googleMap {
+      width: 100%;
+      height: 400px;
+      -webkit-filter: grayscale(100%);
+      filter: grayscale(100%);
+  }
+  .navbar {
+      font-family: Montserrat, sans-serif;
+      margin-bottom: 0;
+      background-color: #2d2d30;
+      border: 0;
+      font-size: 11px !important;
+      letter-spacing: 4px;
+      opacity: 0.9;
+  }
+  .navbar li a, .navbar .navbar-brand {
+      color: #d5d5d5 !important;
+  }
+  .navbar-nav li a:hover {
+      color: #fff !important;
+  }
+  .navbar-nav li.active a {
+      color: #fff !important;
+      background-color: #29292c !important;
+  }
+  .navbar-default .navbar-toggle {
+      border-color: transparent;
+  }
+  .open .dropdown-toggle {
+      color: #fff;
+      background-color: #555 !important;
+  }
+  .dropdown-menu li a {
+      color: #000 !important;
+  }
+  .dropdown-menu li a:hover {
+      background-color: red !important;
+  }
+  footer {
+      background-color: #2d2d30;
+      color: #f5f5f5;
+      padding: 32px;
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      left: 0;
+  }
+  footer a {
+      color: #f5f5f5;
+  }
+  footer a:hover {
+      color: #777;
+      text-decoration: none;
+  }
+  .form-control {
+      border-radius: 0;
+  }
+  textarea {
+      resize: none;
+  }
+  .form-control {
+      border-radius: 0;
+  }
+  textarea {
+      resize: none;
+  }
+  html,body {
+ 
+ 
+  text-align:center;
+}
+
+h1 {
+  font-size: 3em;
+  font-weight: 200;
+  margin: 0.5em 0 0.2em 0;
+}
+
+p {
+  margin: 1.5em 0;
+ 
+}
+
+.thumbnail {
+  max-width: 40%;
+}
+
+
+/** LIGHTBOX MARKUP **/
+
+.lightbox {
+  /** Default lightbox to hidden */
+  display: none;
+
+  /** Position and style */
+  position: fixed;
+  z-index: 999;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  top: 100px;
+  left: 0;
+  
+}
+
+.lightbox img {
+  /** Pad the lightbox image */
+  max-width: 200%;
+  max-height: 190%;
+  margin-top: 2%;
+  text-align: center;
+}
+
+.lightbox:target {
+  /** Remove default browser outline */
+  outline: none;
+
+  /** Unhide lightbox **/
+  display: block;
+}
+  </style>
+
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#myPage">Charif Cherkaoui</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+      
+ 
+       
+            <li><a href="#">LOG UIT</a></li>
+          </ul>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+
+ 
+
+    <!-- Wrapper for slides 
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="code-1076533_960_720" alt="New York" width="1200" height="700">
+        <div class="carousel-caption">
+          <h3>New York</h3>
+          <p>The atmosphere in New York is lorem ipsum.</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="chicago.jpg" alt="Chicago" width="1200" height="700">
+        <div class="carousel-caption">
+          <h3>Chicago</h3>
+          <p>Thank you, Chicago - A night we won't forget.</p>
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="la.jpg" alt="Los Angeles" width="1200" height="700">
+        <div class="carousel-caption">
+          <h3>LA</h3>
+          <p>Even though the traffic was a mess, we had the best time playing at Venice Beach!</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Left and right controls -
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+</div> --> 
+
+<!-- Container  -->
+
+<center>
+<div id="band" class="container text-center">
+<h1>Login</h1>
+
+<form action="" method="POST">
+<input type="hidden" name="loginform" value="1">
+<input type="text" name="gebruikersnaam" placeholder="Je gebruikersnaam">
+<input type="password" name="wachtwoord" placeholder="Je wachtwoord">
+<input type="submit" name="submit" value="login">
+</form>
+
+
+<br>
+<br>
+
+<br>
+<br>
+<h1> Registeer</h1>
+
+<form action="" method="POST">
+<input type="hidden" name="registerform" value="1">
+<label for="txt_gebruikersnaam">gebruikersnaam</label>
+<input type="text" id="text_gebruikersnaam" name="gebruikersnaam"> 
+  <br/> 
+  <label for="txt_wachtwoord">wachtwoord</label>
+  <input type="password" id"txt_wachtwoord" name="wachtwoord" placeholder="een wachtwoord">
+  <br/>
+  <input type="submit" name="submit" value="registeren">
+</form>
+<br>
+ <br>
+ <br>
+ <br>
+ <br>
+ 
+
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">klik hier voor de code van de inlog</button> 
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Klik op de foto om in te zoomen</h4>
+        </div>
+        <div class="modal-body">
+
+          <p><strong>dit is de code die ik heb gemaakt om in te loggen</strong></p>
+           <a href="#img1">
+  <img src="login.png" class="thumbnail">
+</a>
+
+<!-- lightbox container hidden with CSS -->
+<a href="#_" class="lightbox" id="img1">
+  <img src="login.png">
+</a>
+
+       
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Sluit</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+</div>
+<br>
+
+
+
+
+<!-- Footer -->
+<footer class="text-center">
+  <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a><br><br>
+  <p>Copyright &copy; Charif Cherkaoui 2016</p>
+</footer>
+
+<script>
+$(document).ready(function(){
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+  
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+})
+</script>
+</center>
+</body>
+</html>
+
